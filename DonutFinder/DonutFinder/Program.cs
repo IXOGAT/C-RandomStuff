bb244fb6661d309fcd71ecc256cb287973d7c26c
Alex <almo121@live.com>
2017-10-04 22:45:07 -0700

    it works! except for the group section thingy.... that's a problem. will sort that out. Need to also add caloric information. just numbers that big would be sick.

--- DonutFinder/DonutFinder/Program.cs
+++ DonutFinder/DonutFinder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DonutFinder
 {
@@ -13,29 +14,33 @@ namespace DonutFinder
             Boolean multiSet = false;
             int people = 0;
             int donuts = 0;
+            string tmp2 = "1";
             //puts every line of the .txt files into a string array
-            string[] regDonuts = System.IO.File.ReadAllLines(@"D:\Users\temp.MOO\Source\Repos\C-RandomStuff\DonutFinder\DonutFinder\Donuts.txt");
-            string[] expreDonuts = System.IO.File.ReadAllLines(@"D:\Users\temp.MOO\Source\Repos\C-RandomStuff\DonutFinder\DonutFinder\ExpressionDonuts.txt");
+            string regDonutsPath = @"D:\Users\temp.MOO\Source\Repos\C-RandomStuff\DonutFinder\DonutFinder\Donuts.txt";
+            string expreDonutsPath = @"D:\Users\temp.MOO\Source\Repos\C-RandomStuff\DonutFinder\DonutFinder\ExpressionDonuts.txt";
+            string outputDonutsPath = @"D:\Users\temp.MOO\Source\Repos\C-RandomStuff\DonutFinder\DonutFinder\OutputDonuts.txt";
+            string[] regDonuts = System.IO.File.ReadAllLines(regDonutsPath);
+            string[] expreDonuts = System.IO.File.ReadAllLines(expreDonutsPath);
             //ask the user if they want it in random sets (do you want everyone to have the same random donuts?)
             Console.WriteLine("Are you serving multiple people with these donuts? If so, do you want everyone to get the same random set of donuts?");
             String in1 = Console.ReadLine();
             in1 = in1.ToLower();
-            if(in1.Contains("yes"))
+            if (in1.Contains("yes"))
             {
                 multiSet = true;
-            Inquiry1:
+                Inquiry1:
                 Console.WriteLine("How many donuts do you want each person to recieve?");
                 string tmp = Console.ReadLine();
-                if(Int32.TryParse(tmp, out donuts))
+                if (Int32.TryParse(tmp, out donuts))
                 {
-                Inquiry2:
+                    Inquiry2:
                     Console.WriteLine("OK. You want each person to get " + donuts + " donuts. How many people will there be for this rad party?!?");
                     tmp = Console.ReadLine();
                     people = 0;
-                    if(Int32.TryParse(tmp, out people))
+                    if (Int32.TryParse(tmp, out people))
                     {
-                        int tmpDonuts = donuts * people;
-                        Console.WriteLine("This means that you want to order " + tmpDonuts + " donuts? Fine by me.");
+                        donuts = donuts * people;
+                        Console.WriteLine("This means that you want to order " + donuts + " donuts? Fine by me.");
                     }
                     else
                     {
@@ -52,36 +57,37 @@ namespace DonutFinder
             else
             {
                 Console.WriteLine("OK then. All for yourself. Sounds good.");
-            Inquiry3:
+                Inquiry3:
                 Console.WriteLine("How many circles of wonder do you want?");
-                string tmp = Console.ReadLine();
-                if(Int32.TryParse(tmp, out donuts))
-                {
-                    Console.WriteLine("Allrighty! I will now generate a list of " + donuts + " random Krispy Kreme donuts!");
-                    Console.WriteLine("GENERATING!");
-                    Random rnd = new Random();
-                    int realisticCount = 0;
-                    if (donuts > 47)
-                    {
-                        realisticCount = 47;
-                    }
-                    else
-                    {
-                        realisticCount = donuts;
-                    }
-                    for (int i = 0; i <= donuts; i++)
-                    {
-                        System.IO.File.WriteAllText(@"D:\Users\temp.MOO\Source\Repos\C-RandomStuff\DonutFinder\DonutFinder\OutputDonuts.txt", regDonuts[rnd.Next(realisticCount)]);
-                    }
-                    Console.WriteLine("Donut generation complete. Would you like to see the list of donuts?");
-                }
-                else
+                tmp2 = Console.ReadLine();
+                if (!Int32.TryParse(tmp2, out donuts))
                 {
                     Console.WriteLine("Sorry. I didn't quite get that. Try again please.");
                     goto Inquiry3;
                 }
             }
-
+            Console.WriteLine("Allrighty! I will now generate a list of " + donuts + " random Krispy Kreme donuts!");
+            Console.WriteLine("GENERATING!");
+            Random rnd = new Random();
+            int realisticCount = 0;
+            if (donuts > 47)
+            {
+                realisticCount = 47;
+            }
+            else
+            {
+                realisticCount = donuts;
+            }
+            StreamWriter sw = File.AppendText(outputDonutsPath);
+            for (int i = 0; i <= donuts; i++)
+            {
+                        
+                sw.WriteLine(regDonuts[rnd.Next(realisticCount)]);
+                Console.WriteLine(regDonuts[rnd.Next(realisticCount)]);
+            }
+            Console.WriteLine("Donut generation complete. Would you like to see the list of donuts?");
+            Console.ReadLine();
+            Console.ReadLine();
             //if yes, then ask how many donuts, and how many per person
 
             //if no, then ask how many donuts
